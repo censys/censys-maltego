5d95cf3cc28c63b8550cb3e330a0ae86e7a369a3
Aidan Holland <aidan@censys.io>
2021-04-20 16:26:52 -0400

    feat: IPv4 to Location

--- src/censys_maltego/transforms/common/utils.py
+++ src/censys_maltego/transforms/common/utils.py
@@ -1,20 +1,30 @@
 from canari.maltego.entities import Location
+from canari.maltego.message import Field
 
 LOCATION_FIELDS = [
     "location.latitude",
     "location.longitude",
     "location.country",
     "location.country_code",
+    "location.city",
+    "location.province",
+    "location.timezone",
 ]
 
 
 def generate_location(res: dict):
-    return Location(
+    location = Location(
         country=res.get("location.country", "Unknown"),
         countrycode=res.get("location.country_code"),
         latitude=res.get("location.latitude"),
         longitude=res.get("location.longitude"),
+        city=res.get("location.city"),
+        area=res.get("location.province"),
     )
+    timezone = res.get("location.timezone")
+    if timezone:
+        location += Field("location.timezone", timezone, display_name="Timezone")
+    return location
 
 
 def check_api_creds(request, response, config):
---
+++ src/censys_maltego/transforms/ipaddresstolocation.py
@@ -0,0 +1,53 @@
+from canari.maltego.entities import IPv4Address
+from canari.maltego.transform import Transform
+from canari.maltego.message import MaltegoException
+from canari.framework import RequestFilter
+
+from censys_maltego.transforms.common.utils import (
+    check_api_creds,
+    LOCATION_FIELDS,
+    generate_location,
+)
+
+__author__ = "Censys Team"
+__copyright__ = "Copyright 2021, censys_maltego Project"
+__credits__ = ["Aidan Holland"]
+
+__license__ = "GPLv3"
+__version__ = "0.1"
+__maintainer__ = "Censys Team"
+__email__ = "support@censys.io"
+__status__ = "Development"
+
+
+@RequestFilter(check_api_creds)
+class IPAddressToLocation(Transform):
+    """IPv4 Address to Location."""
+
+    # The transform input entity type.
+    input_type = IPv4Address
+
+    def do_transform(self, request, response, config):
+        from censys import CensysIPv4
+
+        c = CensysIPv4()
+        # c = CensysIPv4(config["censys.local.api_id"], config["censys.local.api_secret"])
+        ip = request.entity.value
+        res = list(
+            c.search(
+                f"ip: {ip} AND location.country: *",
+                fields=LOCATION_FIELDS,
+                max_records=1,
+            )
+        )
+        if len(res) == 0:
+            raise MaltegoException(f"No search results found for {ip}")
+
+        response += generate_location(res[0])
+
+        return response
+
+    def on_terminate(self):
+        """This method gets called when transform execution is prematurely terminated. It is only applicable for local
+        transforms. It can be excluded if you don't need it."""
+        pass
