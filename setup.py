d71896e407f7f2fc4b2f713bc1ce9b8a1054e887
Aidan Holland <aidan@censys.io>
2021-04-21 11:42:03 -0400

    chore: set beta version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="censys_maltego",
-    version="2.0.0",
+    version="2.0.0.b1",
     description="This package provides an interface into Censys from Maltego.",
     long_description=long_description,
     long_description_content_type="text/markdown",
