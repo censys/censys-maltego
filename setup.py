6822e55552217c7309c824c7f62a00611aa0026d
Aidan Holland <aidan@censys.io>
2021-04-21 09:37:16 -0400

    chore: Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
-
 from setuptools import setup, find_packages
 
+GIT_URL = "https://github.com/censys/censys-maltego"
+
 setup(
-    name='censys_maltego',
-    author='Censys Team',
-    version='2.0.0',
-    author_email='support@censys.io',
-    description='This package provides an interface into Censys from Maltego.',
-    license='Apache License, Version 2.0',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    name="censys_maltego",
+    author="Censys Team",
+    version="2.0.0",
+    author_email="support@censys.io",
+    description="This package provides an interface into Censys from Maltego.",
+    license="Apache License, Version 2.0",
+    python_requires=">=3.6.0",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     zip_safe=False,
     package_data={
-        '': ['*.gif', '*.png', '*.conf', '*.mtz', '*.machine']  # list of resources
+        "": ["*.gif", "*.png", "*.conf", "*.mtz", "*.machine"]  # list of resources
     },
     install_requires=[
-        'canari>=3.3.10,<4',
-        'censys==2.0.0b1',
+        "canari>=3.3.10,<4",
+        "censys==2.0.0b1",
     ],
     extras_require={
         "dev": [
@@ -31,5 +33,30 @@ setup(
             "black==20.8b1",
             "mypy==0.812",
         ]
-    }
+    },
+    classifiers=[
+        "Topic :: Internet",
+        "Topic :: Security",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Flake8",
+        "Natural Language :: English",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    project_urls={
+        "Censys Homepage": "https://censys.io/",
+        "Changelog": GIT_URL + "/releases",
+        "Tracker": GIT_URL + "/issues",
+        "Source": GIT_URL,
+    },
 )
